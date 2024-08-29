f466c7a2de877b59bd002ce490501287418e60db
June <fromhelianthus@gmail.com>
2024-08-30 00:38:58 +0900

    feat server socket emit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,44 +13,57 @@ let roomName;
 function addMessage(message) {
     const ul = room.querySelector("ul");
     const li = document.createElement("li");
+
     li.innerText = message;
     ul.appendChild(li);
 }
 
 function handleMessageSubmit(event) {
     event.preventDefault();
-    const input = room.querySelector("input");
+
+    const input = room.querySelector("#msg input");
     const value = input.value;
+
     socket.emit("new_message", input.value, roomName, () => {
         addMessage(`You: ${value}`);
     });
+
     input.value = "";
 }
 
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
+
     const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName}`;
-    const form = room.querySelector("form");
-    form.addEventListener("submit", handleMessageSubmit);
+    h3.innerText = `Room: ${roomName}`;
+
+    const msgForm = room.querySelector("#msg");
+    msgForm.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event) {
     event.preventDefault();
+
     const input = form.querySelector("input");
     socket.emit("enter_room", input.value, showRoom);
+
     roomName = input.value;
+    input.value = "";
 }
 
 form.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", () => {
-    addMessage("someone joined!");
+socket.on("welcome", (user, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room: ${roomName} (${newCount})`;
+    addMessage(`${user} arrived!`);
 });
 
-socket.on("bye", () => {
-    addMessage("someone left ㅠㅠ");
+socket.on("bye", (left, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room: ${roomName} (${newCount})`;
+    addMessage(`${left} left...`);
 });
 
 socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -6,37 +6,69 @@ import SocketIO from "socket.io";
 
 const app = express();
 
-// Set the view engine to Pug
-// The folder name for pug must be 'views'
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-// Route for the home page
-// Only use the root directory ('/') for rendering
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: { sids, rooms },
+        },
+    } = wsServer;
+
+    const publicRooms = [];
+
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+
+    return publicRooms;
+}
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", (socket) => {
+    socket["nickname"] = "Anon";
+
     socket.onAny((event) => {
         console.log(`Socket Event: ${event}`);
     });
+
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome");
+        socket.to(roomName).emit("welcome", socket.nickname);
+        wsServer.sockets.emit("room_change", publicRooms());
     });
+
     socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+        socket.rooms.forEach((room) =>
+            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
+        );
     });
+
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
+
     socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", msg);
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
     });
+
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 const handleListen = () => console.log(`http://localhost:3000`);
