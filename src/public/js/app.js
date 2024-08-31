2f3261ab5c095d2228ba7e18dbe6ac464789d986
June <fromhelianthus@gmail.com>
2024-09-01 07:50:25 +0900

    feat Data Channel

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,6 +16,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
     try {
@@ -23,7 +24,6 @@ async function getCameras() {
         const cameras = devices.filter(
             (device) => device.kind === "videoinput"
         );
-
         const currentCamera = myStream.getVideoTracks()[0];
 
         cameras.forEach((camera) => {
@@ -138,6 +138,11 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 // Socket
 
 socket.on("welcome", async () => {
+    myDataChannel = myPeerConnection.createDataChannel("chat");
+    myDataChannel.addEventListener("message", (event) =>
+        console.log(event.data)
+    );
+    console.log("made data channel");
     const offer = await myPeerConnection.createOffer();
     myPeerConnection.setLocalDescription(offer);
     console.log("sent the offer");
@@ -145,9 +150,14 @@ socket.on("welcome", async () => {
 });
 
 socket.on("offer", async (offer) => {
+    myPeerConnection.addEventListener("datachannel", (event) => {
+        myDataChannel = event.channel;
+        myDataChannel.addEventListener("message", (event) =>
+            console.log(event.data)
+        );
+    });
     console.log("received the offer");
     myPeerConnection.setRemoteDescription(offer);
-
     const answer = await myPeerConnection.createAnswer();
     myPeerConnection.setLocalDescription(answer);
     socket.emit("answer", answer, roomName);
